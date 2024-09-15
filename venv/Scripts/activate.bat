81a0530ceb2b4548f509fa31054c25e4462677b4
VVILEL <93390996+VVILEL@users.noreply.github.com>
2024-09-15 09:33:12 -0500

    Change

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Joaquin Diaz\Desktop\AutDetect\autdetect-backend\venv
+set VIRTUAL_ENV=C:\Users\User\Documents\GitHub\autdetect-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
