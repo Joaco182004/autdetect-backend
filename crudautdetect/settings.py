e8037006ac3957dc38b96491180e1f1de99cc04d
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2025-03-27 01:10:43 -0500

    change

--- crudautdetect/settings.py
+++ crudautdetect/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors authorization
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5174"
 ]
 
 REST_FRAMEWORK = {
